3a5254d316c733620f5ff7df8da0475c1a49b604
David Colwell <d.colwell@tricentis.com>
2018-04-23 11:53:49 +1000

    Added new test project for BDD example code

---
+++ TosCode/BddExample/TestStepDefinitions.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BddExample
+{
+    public class TestStepDefinitions
+    {
+        IWebDriver driver;
+        public TestStepDefinitions()
+        {
+            string chromeDriverPath = @"C:\Temp\ChromeDriver";
+            ChromeOptions options = new ChromeOptions();
+            string localPath = Environment.ExpandEnvironmentVariables("%APPDATA%");
+            options.AddArguments($@"user-data-dir={localPath}\Google\Chrome\User Data\Selenium");
+            driver = new ChromeDriver(chromeDriverPath, options);
+        }
+
+        public void OpenSampleApp(string url)
+        {
+            driver.Url = url;
+            driver.Navigate();
+        }
+    }
+}
